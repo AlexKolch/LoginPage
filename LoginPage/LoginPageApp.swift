c022786142da80f7b13eed859ed97a4f9b342ef7
Alexey Kolch <kolychenkov94@mail.ru>
2023-09-29 15:27:01 +0400

    Initial Commit

---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by Алексей Колыченков on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
